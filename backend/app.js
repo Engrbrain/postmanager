862d87603cbf986a649e56b917a485c1336fa419
Uhunghama Harrison <engrbrain@gmail.com>
2021-10-29 20:13:12 +0100

    adding dummy data from node

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-console.log('First Middleware')
-next();
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {id:'jskjsj987', title: 'First Server-Side Post', content: 'This is coming from the server'},
+    {id:'ishsghj34', title: 'Second Server-Side Post', content: 'This is coming from the server'},
+    {id:'kjashysk2', title: 'Third Server-Side Post', content: 'This is coming from the server'}
+  ]
+res.status(200).json({
+  message: 'Posts fetched successfully',
+  posts: posts
+});
 });
 
-app.use((req, res, next) => {
-  res.send('Hello from express');
-});
+// app.use((req, res, next) => {
+//   res.send('Hello from express');
+// });
 
 module.exports = app;
