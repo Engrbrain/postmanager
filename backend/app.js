c6ab6f90ff6aed6e14c8756ef4b075dff15f3a55
Uhunghama Harrison <engrbrain@gmail.com>
2021-11-04 13:36:17 +0100

    creating a post instance

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Post = require('./models/post')
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,7 +16,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body; // This will return one post
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  })
   console.log(post);
   res.status(201).json({
     message: 'Post added Successfully'
