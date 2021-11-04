5d5a360386e26aadafb403f391d058fc4639bb49
Uhunghama Harrison <engrbrain@gmail.com>
2021-11-04 14:46:19 +0100

    Transforming backend return data from the front end using rxjs

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,18 +2,29 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import {Post} from './post.model';
 import {HttpClient} from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
-  private posts: Post[] = [];
+  private posts: any;
   private postsUpdated = new Subject<Post[]>();
 
   constructor(private http: HttpClient) {}
 
   getPosts(){
-    this.http.get<{message:string, posts: Post[]}>('http://localhost:3000/api/posts').
-    subscribe((postData) => {
-      this.posts = postData.posts;
+    this.http.get<{message:string, posts: any}>('http://localhost:3000/api/posts')
+    .pipe(map((postData) => {
+      return postData.posts.map(post => {
+        return{
+          title: post.title,
+          content: post.content,
+          id: post._id
+        }
+      })
+
+    }))
+    .subscribe((transformedPosts) => {
+      this.posts = transformedPosts;
       this.postsUpdated.next([...this.posts]);
     });
   }
