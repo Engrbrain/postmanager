9997275e0377677f70bac59d8fc3d6e8d269c732
Uhunghama Harrison <engrbrain@gmail.com>
2021-11-05 14:38:14 +0100

    Added CORS, Added Delete Route, Updated ID after delete

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const Post = require('./models/post')
 
@@ -16,23 +17,33 @@ mongoose.connect("mongodb+srv://engrbrain:Fetele0301776@maincluster.1xeov.mongod
   console.log('Connection Failed')
 });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers",
-  "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Acess-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
-  next();
-});
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers",
+//   "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   res.header("Acess-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+//   if (res.Method == "OPTIONS" || res.Method == "DELETE") {
+//     res.header("http.StatusOK")
+//     return
+//   }
+//   next();
+// });
+
 
 app.post("/api/posts", (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
   })
-  post.save();
-  res.status(201).json({
-    message: 'Post added Successfully'
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added Successfully',
+      postId: createdPost._id
+    });
   });
+
 });
 
 app.get('/api/posts', (req, res, next) => {
@@ -43,6 +54,15 @@ app.get('/api/posts', (req, res, next) => {
     });
   });
 });
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
+  });
+});
+
+
 
 // app.use((req, res, next) => {
 //   res.send('Hello from express');
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -11,22 +11,24 @@ export class PostsService {
 
   constructor(private http: HttpClient) {}
 
-  getPosts(){
-    this.http.get<{message:string, posts: any}>('http://localhost:3000/api/posts')
-    .pipe(map((postData) => {
-      return postData.posts.map(post => {
-        return{
-          title: post.title,
-          content: post.content,
-          id: post._id
-        }
-      })
-
-    }))
-    .subscribe((transformedPosts) => {
-      this.posts = transformedPosts;
-      this.postsUpdated.next([...this.posts]);
-    });
+  getPosts() {
+    this.http
+      .get<{ message: string; posts: any }>(
+        "http://localhost:3000/api/posts"
+      )
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe(transformedPosts => {
+        this.posts = transformedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener(){
@@ -35,12 +37,25 @@ export class PostsService {
 
   addPost(title: string, content: string){
     var id = Math.random().toString(36).slice(2);
-    const post: Post = {id, title: title, content: content};
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post).subscribe((responseData) => {
-      console.log(responseData.message);
+    const post: Post = {id: id, title: title, content: content};
+    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
+    .subscribe((responseData) => {
+      const id = responseData.postId;
+      post.id = id;
       this.posts.push(post);
       this.postsUpdated.next([...this.posts]);
     });
 
   }
+
+  deletePost(postId: string) {
+    this.http.delete("http://localhost:3000/api/posts/" + postId)
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
 }
+
+
