bec43da97f0449a2afad2edc9eee0dad307eea2d
Uhunghama Harrison <engrbrain@gmail.com>
2021-11-24 08:20:28 +0100

    Create Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,12 +18,14 @@ import {PostCreateComponent} from './posts/post-create/post-create.component';
 import {HeaderComponent} from './header/header.component';
 import {PostListComponent} from './posts/post-list/post-list.component'
 import {MatPaginatorModule} from '@angular/material/paginator';
+import { LoginComponent } from './auth/login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     HeaderComponent,
-    PostListComponent
+    PostListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
