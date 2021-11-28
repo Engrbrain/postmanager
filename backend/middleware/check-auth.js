a742437047ee3cf1c4eaab7a6dc10eef41f144cb
Uhunghama Harrison <engrbrain@gmail.com>
2021-11-28 09:12:13 +0100

    Adding the User ID to Posts

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try{
     const token = req.header.authorization.split(" ")[1];
-    jwt.verify(token, "Awnkm0akm?##Fetele03017761988%%D3fault100$%%");
+    const decodedToken = jwt.verify(token, "Awnkm0akm?##Fetele03017761988%%D3fault100$%%");
+    req.userData = { email: decodedToken.email, userId: decodedToken.userId }
     next();
   } catch (error) {
     res.status(401).json({
